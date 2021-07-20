dc259d7d87c876ef5a07c26ef21dc0c5abf715f7
marialubarbosa <barbosamarialu17@gmail.com>
2021-07-19 22:00:38 -0300

    adding-categories-in-memory-database

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,14 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class InMemoryDbService implements InMemoryDbService{
+    createDb(){
+        const categories = [
+            {id: 1, name: "Moradia", description: "Pagamentos de Contas da Casa"},
+            {id: 1, name: "Saúde", description: "Plano de Saúde e Remédios"},
+            {id: 1, name: "Lazer", description: "Cinema, parques, praia e etc"},
+            {id: 1, name: "Salário", description: "Recebimento Salário"},
+            {id: 1, name: "Freelas", description: "Trabalhos como freelancer"}
+        ];
+        return { categories }
+    }
+}
